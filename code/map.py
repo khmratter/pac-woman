378dcddf6f2e7489f96a964e5f4bd3e17724c616
Szyszkolot <anna.leszczynn@gmail.com>
2025-06-21 14:16:05 +0200

    Update map.py

--- code/map.py
+++ code/map.py
@@ -13,6 +13,7 @@ class Map:
         self._add_extra_passages(extra=self.level + 3)
         self._break_long_walls(max_len=3)
         self._place_points()
+        self._mark_accessible()
 
     # Generuje labirynt metodą DFS
     def _gen_maze(self):
@@ -22,26 +23,22 @@ class Map:
                 t = self.grid[y][x]
                 t.wall_top = t.wall_bottom = t.wall_left = t.wall_right = True
 
-        dirs = [
-            ("top", 0, -1, "bottom"),
-            ("bottom", 0, 1, "top"),
-            ("left", -1, 0, "right"),
-            ("right", 1, 0, "left"),
-        ]
-        visited = [[False] * size for _ in range(size)]
+        dirs = [('top',0,-1,'bottom'),('bottom',0,1,'top'),
+                ('left',-1,0,'right'),('right',1,0,'left')]
+        visited = [[False]*size for _ in range(size)]
 
         def dfs(x, y):
             visited[y][x] = True
             dir_order = dirs[:]
             random.shuffle(dir_order)
             for w, dx, dy, ow in dir_order:
-                steps = random.randint(1, 2)
+                steps = random.randint(1, 3)
                 cx, cy = x, y
                 for _ in range(steps):
                     nx, ny = cx + dx, cy + dy
                     if 0 <= nx < size and 0 <= ny < size and not visited[ny][nx]:
-                        setattr(self.grid[cy][cx], f"wall_{w}", False)
-                        setattr(self.grid[ny][nx], f"wall_{ow}", False)
+                        setattr(self.grid[cy][cx], f'wall_{w}', False)
+                        setattr(self.grid[ny][nx], f'wall_{ow}', False)
                         visited[ny][nx] = True
                         dfs(nx, ny)
                         break
@@ -51,49 +48,47 @@ class Map:
         dfs(self.size // 2, self.size // 2)
 
     # Dodaje losowe dodatkowe przejścia
-    def _add_extra_passages(self, extra=None):
-        if extra is None:
-            extra = (self.size * 3) + 3
-            count = 0
-            size = self.size
-            while count < extra:
-                x = random.randint(0, size - 2)
-                y = random.randint(0, size - 2)
-                t1 = self.grid[y][x]
-                if random.choice([True, False]):
-                    t2 = self.grid[y][x + 1]
-                    if t1.wall_right and t2.wall_left:
-                        t1.wall_right = False
-                        t2.wall_left = False
-                        count += 1
-                else:
-                    t2 = self.grid[y + 1][x]
-                    if t1.wall_bottom and t2.wall_top:
-                        t1.wall_bottom = False
-                        t2.wall_top = False
-                        count += 1
+    def _add_extra_passages(self, extra=5):
+        count = 0
+        size = self.size
+        while count < extra:
+            x = random.randint(0, size-2)
+            y = random.randint(0, size-2)
+            t1 = self.grid[y][x]
+            if random.choice([True, False]):
+                t2 = self.grid[y][x+1]
+                if t1.wall_right and t2.wall_left:
+                    t1.wall_right = False
+                    t2.wall_left = False
+                    count += 1
+            else:
+                t2 = self.grid[y+1][x]
+                if t1.wall_bottom and t2.wall_top:
+                    t1.wall_bottom = False
+                    t2.wall_top = False
+                    count += 1
 
     # Przerywa zbyt długie ściany
-    def _break_long_walls(self, max_len=1):
+    def _break_long_walls(self, max_len=3):
         for y in range(self.size):
             count = 0
             for x in range(self.size - 1):
-                if self.grid[y][x].wall_right and self.grid[y][x + 1].wall_left:
+                if self.grid[y][x].wall_right and self.grid[y][x+1].wall_left:
                     count += 1
                     if count >= max_len:
                         self.grid[y][x].wall_right = False
-                        self.grid[y][x + 1].wall_left = False
+                        self.grid[y][x+1].wall_left = False
                         count = 0
                 else:
                     count = 0
         for x in range(self.size):
             count = 0
             for y in range(self.size - 1):
-                if self.grid[y][x].wall_bottom and self.grid[y + 1][x].wall_top:
+                if self.grid[y][x].wall_bottom and self.grid[y+1][x].wall_top:
                     count += 1
                     if count >= max_len:
                         self.grid[y][x].wall_bottom = False
-                        self.grid[y + 1][x].wall_top = False
+                        self.grid[y+1][x].wall_top = False
                         count = 0
                 else:
                     count = 0
@@ -107,6 +102,9 @@ class Map:
                 if (x, y) == (start_x, start_y):
                     continue  # pomijamy pole startowe gracza
                 self.grid[y][x].point = True
+
+
+    
 
     # Na razie pusta – można użyć np. do oznaczenia dostępnych pól
     def _mark_accessible(self):
